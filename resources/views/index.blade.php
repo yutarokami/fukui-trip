90a92b09997726249e6c52729cf388b403253b14
yutarokami <kamitch01@gmail.com>
2022-09-09 08:20:16 +0900

    indexファイル(TopページのView)を作成

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,1 +1,81 @@
-Hello,world
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>FUKUI TRIP</title>
+        <link rel="stylesheet" href="css/index.css">
+        <!-- googlefont -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Cormorant+Unicase:wght@300;400;500;600;700&family=Noto+Sans+JP:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
+    </head>
+
+    <body>
+        <div class="top">
+            <h1>FUKUI TRIP</h1>
+            
+            <div class="top_left_block">    
+                <img src="/img/top_image_left.jpg">
+                <h3>最近、疲れがたまっているな。<br>都会もいいけど、たまにはゆっくりしたいな。<br>そんなときは、福井がききます。</h3>
+            </div>
+            
+            <div class="top_right_block">
+                <div class="top_right_block1">
+                    <h2 class="sub_title">しあわせなまち、</br>ふくい。</h2>
+                    <a href="" class="menu_btn">MENU</a>
+                </div>
+                <img src="\img\top_image_right.jpg">      
+            </div>
+        </div>
+
+        <div class="high_rated">
+            <div class="high_rated_left">
+                <h2 class="high_rated_title">高評価</h2>
+                <img src="\img\high_rated_left.jpg">
+            </div>
+
+            <div class="high_rated_center">
+                <img src="\img\high_rated_center.jpg">
+            </div>
+
+            <div class="high_rated_right">
+                <img src="\img\high_rated_right.jpg">
+                <a href="" class="high_other_page">
+                    <h2>高評価の投稿を<br>見てみる</h2>
+                    <div class="triangle_btn">
+                    </div>
+                </a>
+            </div>
+        </div>
+
+        <div class="line"></div>
+
+        <div class="category">
+            <div class="category_top">
+                <div class="category_top1">
+                    <h1>sea</h1>
+                    <img src="\img\category_top1.jpg">
+                </div>
+                <div class="category_top2">
+                    <h1>moutain</h1>
+                    <img src="\img\category_top2.jpg">
+                </div>
+            </div>
+
+            <div class="category_bottom">
+                <h2 class="category_title">カテゴリ別</h2>
+                <div class="category_bottom1">
+                    <h1>dinosaur</h1>
+                    <img src="\img\category_bottom.jpg">
+                </div>
+                <div class="category_bottom2">
+                    <a href="" class="category_other_page">
+                    <h2>人気のカテゴリ<br>一覧</h2>
+                    <div class="triangle_btn">
+                    </div>
+                    </a>
+                </div>
+            </div>
+
+        </div>
+    </body>
+</html>
