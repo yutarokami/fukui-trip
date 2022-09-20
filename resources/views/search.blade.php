03daf1f5f00c1e33d46d066f615b78a6521ae19b
yutarokami <kamitch01@gmail.com>
2022-09-20 23:38:34 +0900

    Searchページの実装

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Search</title>
-    <link rel="stylesheet" href="css/login.css">
+    <link rel="stylesheet" href="css/search.css">
     <!-- fontawesome -->
     <script src="https://kit.fontawesome.com/b56d778a44.js" crossorigin="anonymous"></script>
     <!-- googlefont -->
@@ -19,5 +19,135 @@
 </head>
 
 <body>
-    
+    <header>
+        <h2 class="sub_title">しあわせなまち、</br>ふくい。</h2>
+        <a href="/index" class="back_btn">BACK</a>
+    </header>
+
+    <div class="menu_title">
+        <div class="title">
+            <h2 class="title_en">SEARCH</h2>
+            <h2 class="title_ja">探す</h2>
+        </div>
+        <img src="\img\search.jpg">
+    </div>
+
+    <div class="main_block">
+        <!-- category_group部分のhtml -->
+        <div class="category_group">
+            <h3 class="category_title">カテゴリ別</h3>
+            <div class="list">
+                <ul>
+                    <li>
+                        <img src="\img\category_group\絶景.jpg">
+                        <a href="">絶景</a>
+                    </li>
+                    <li>
+                        <img src="\img\category_group\グルメ.jpg">
+                        <a href="">グルメ</a>
+                    </li>
+                    <li>
+                        <img src="\img\category_group\温泉.jpg">
+                        <a href="">温泉</a>
+                    </li>
+                    <li>
+                        <img src="\img\category_group\観光.jpg">
+                        <a href="">観光</a>
+                    </li>
+                    <li>
+                        <img src="\img\category_group\旅館.jpg">
+                        <a href="">旅館</a>
+                    </li>
+                    <li>
+                        <img src="\img\category_group\居酒屋.jpg">
+                        <a href="">居酒屋</a>
+                    </li>
+                    <li>
+                        <img src="\img\category_group\スイーツ.jpg">
+                        <a href="">スイーツ</a>
+                    </li>
+                    <li>
+                        <img src="\img\category_group\カフェ.jpg">
+                        <a href="">カフェ</a>
+                    </li>
+                    <li>
+                        <img src="\img\category_group\この秋行きたい.jpg">
+                        <a href="">この秋行きたい</a>
+                    </li>
+                    <li>
+                        <img src="\img\category_group\おみやげ.jpg">
+                        <a href="">おみやげ</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="category_button">
+                <a href="" class="category_other_page">
+                    <h4>ほかのカテゴリを見る</h4>
+                    <div class="triangle_btn">
+                    </div>
+                </a>
+            </div>
+        </div>
+
+        <!-- area_group部分のhtml -->
+        <div class="area_group">
+            <h3 class="area_title">地域別</h3>
+            <div class="list">
+                <ul>
+                    <li>
+                        <img src="\img\area_group\福井市.jpg">
+                        <a href="">福井市</a>
+                    </li>
+                    <li>
+                        <img src="\img\area_group\敦賀市.jpg">
+                        <a href="">敦賀市</a>
+                    </li>
+                    <li>
+                        <img src="\img\area_group\あわら市.jpg">
+                        <a href="">あわら市</a>
+                    </li>
+                    <li>
+                        <img src="\img\area_group\勝山市.jpg">
+                        <a href="">勝山市</a>
+                    </li>
+                    <li>
+                        <img src="\img\area_group\坂井市.jpg">
+                        <a href="">坂井市</a>
+                    </li>
+                    <li>
+                        <img src="\img\area_group\鯖江市.jpg">
+                        <a href="">鯖江市</a>
+                    </li>
+                    <li>
+                        <img src="\img\area_group\大野市.jpg">
+                        <a href="">大野市</a>
+                    </li>
+                    <li>
+                        <img src="\img\area_group\小浜市.jpg">
+                        <a href="">小浜市</a>
+                    </li>
+                    <li>
+                        <img src="\img\area_group\美浜町.jpg">
+                        <a href="">美浜町</a>
+                    </li>
+                    <li>
+                        <img src="\img\area_group\おおい町.jpg">
+                        <a href="">おおい町</a>
+                    </li>
+                    <li>
+                        <img src="\img\area_group\南越前町.jpg">
+                        <a href="">南越前町</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="area_button">
+                <a href="" class="area_other_page">
+                    <h4>ほかの地域を見る</h4>
+                    <div class="triangle_btn">
+                    </div>
+                </a>
+            </div>
+        </div>
+
+
 </body>
