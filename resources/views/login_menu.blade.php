19c13411d73be4d91a54182333a0722a05f6f452
yutarokami <kamitch01@gmail.com>
2022-09-23 18:04:17 +0900

    Bootstrap部分を削除

--- resources/views/login_menu.blade.php
+++ resources/views/login_menu.blade.php
@@ -8,12 +8,6 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Cormorant+Unicase:wght@300;400;500;600;700&family=Noto+Sans+JP:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
-
-    <!-- Bootstrap
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet"> -->
-
 </head>
 <body>
     <div class="menu_header">
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -10,12 +10,6 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Cormorant+Unicase:wght@300;400;500;600;700&family=Inter:wght@500&family=Noto+Sans+JP&display=swap" rel="stylesheet">
-
-    <!-- Bootstrap
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet"> -->
-
 </head>
 
 <body>
